fbf5e953084cf0a2cd20b378c8f188008fa286c0
Ismael Thiago Marques <ismael@oc4825278545.ibm.com>
2017-08-22 17:39:22 -0300

    GET

--- movies/app.rb
+++ movies/app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'movie'
 
 get ('/movies') do
-  'coming soon...'
+    @movies = []
+    @movies[0] = Movie.new
+    @movies[0].title = 'Jaws'
+    @movies[1] = Movie.new 
+    @movies[1].title = 'Alien'
+    @movies[2] = Movie.new
+    @movies[2].title = 'terminator 2'
+    erb :index
+end
+
+get ('/movies/new') do
+    erb :new
 end
