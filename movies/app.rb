9671dc4aba3df48750c362a5c952ca3da1a72c1c
tgmrks <tgmrks@gmail.com>
2017-08-22 03:09:06 -0300

    sinatra test finished

--- movies/app.rb
+++ movies/app.rb
@@ -7,18 +7,26 @@ require 'movie_store'
 store = MovieStore.new('movie.yml')
 
 get ('/movies') do
-    @movies = []
-    @movies[0] = Movie.new
-    @movies[0].title = 'Jaws'
-    @movies[1] = Movie.new 
-    @movies[1].title = 'Alien'
-    @movies[2] = Movie.new
-    @movies[2].title = 'terminator 2'
+#    @movies = []
+#    @movies[0] = Movie.new
+#    @movies[0].title = 'Jaws'
+#    @movies[1] = Movie.new 
+#    @movies[1].title = 'Alien'
+#    @movies[2] = Movie.new
+#    @movies[2].title = 'terminator 2'
+    @movies = store.all
     erb :index
 end
 
 get ('/movies/new') do
     erb :new
+end
+
+get ('/movies/:id') do
+    #"Receiveid id: #{params['id']}"
+    id = params['id'].to_i
+    @movie = store.find(id)
+    erb :show
 end
 
 post ('/movies/create') do
